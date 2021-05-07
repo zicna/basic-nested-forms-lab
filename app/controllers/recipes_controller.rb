bb4795088939b02eed7d105253f861505693d1ed
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-05-07 00:48:51 -0500

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,24 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @ingredient = Ingredient.new
   end
 
   def create
+    # raise params.inspect
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      redirect_to recipe_path(@recipe)
+    else
+      render :new
+    end
+    
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,15 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+
+  # accepts_nested_attributes_for :ingredients
+
+  def ingredients_attributes=(ingredients_attributes)
+
+    ingredients_attributes.values.each do |ingredient_attribute|
+      # self.ingredients.build(ingredient_attribute)
+      @ingredient = Ingredient.find_or_create_by(ingredient_attribute)
+      self.ingredients << @ingredient
+    end
+  end
+    
 end
